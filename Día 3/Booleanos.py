436b28d3556efe21a0037431ffe4dbb8082d29c5
Federico Voloschin <fedevoloschin@gmail.com>
2022-04-14 16:10:39 -0300

    clase 3

---
+++ Día 3/Booleanos.py
@@ -0,0 +1,22 @@
+numero = 5 > 2+3
+print(type(numero))
+print(numero)
+
+numero = 5 == 2+3
+print(numero)
+
+numero = 5 <= 2+3
+print(numero)
+
+numero = 5 >= 2+3
+print(numero)
+
+numero = 5 != 2+3
+print(numero)
+
+numero = bool(5<6)
+print(numero)
+
+numero = bool() # bool() es False
+print(numero)
+
